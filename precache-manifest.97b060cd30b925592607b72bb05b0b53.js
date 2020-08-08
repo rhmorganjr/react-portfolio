28a44786cfa42af86d8e081fff23f8e7b90b44a6
Russ Morgan <russmorgan@Russs-Mac-mini.lan1>
2020-08-07 21:16:40 -0500

    Updates

---
+++ precache-manifest.97b060cd30b925592607b72bb05b0b53.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885ceb5834e09d5349f8a2a9102d1d3b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "48ab644f68b34870b7f7",
+    "url": "/react-portfolio/static/css/main.95bc034a.chunk.css"
+  },
+  {
+    "revision": "05d9d68e12d4e2c7fe78",
+    "url": "/react-portfolio/static/js/2.f11bc962.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.f11bc962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ab644f68b34870b7f7",
+    "url": "/react-portfolio/static/js/main.4d66c6c3.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "b8ab51bfe1b2f1152d80a74c707f4913",
+    "url": "/react-portfolio/static/media/IMG_1672.b8ab51bf.jpg"
+  },
+  {
+    "revision": "b029b3931eab951eee6ae1de8f42e1b2",
+    "url": "/react-portfolio/static/media/WeatherDashboard.b029b393.png"
+  },
+  {
+    "revision": "b3046b4b6e4e833be075fa23bea6f854",
+    "url": "/react-portfolio/static/media/code-quiz.b3046b4b.png"
+  },
+  {
+    "revision": "c3aa9e2875d1aa206c45767357114a9e",
+    "url": "/react-portfolio/static/media/nasa-img.c3aa9e28.png"
+  },
+  {
+    "revision": "1536784ee7b879682802bdd42a8b2f9d",
+    "url": "/react-portfolio/static/media/titlepage.1536784e.jpg"
+  }
+]);
